c5e3b8c6f143601110b563f6a12f78dc08b9fd21
NightOwlin27 <144283829+NightOwlin27@users.noreply.github.com>
2025-07-22 14:12:00 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+The purpose of this project is to showcase some intermediate JavaScript skills from the Odin Project.
+
+Showcased skills include:
+
+- Object Constructors
+- Prototypes/Prototypal Inheritance
+- Array Methods
+- CSS Grid
+- Modals
+
+Project assignment/instructions can be found at:
+
+https://www.theodinproject.com/lessons/node-path-javascript-library
---
+++ index.js
@@ -0,0 +1,66 @@
+const myLibrary = [
+    // {
+    //     title: 'dune',
+    //     author: 'frank herbert'
+    // },
+    // {
+    //     title: '1984',
+    //     author: 'george orwell'
+    // }
+];
+
+
+submitBookForm = document.addEventListener('submit', function(e) {
+    title = document.getElementById('title');
+    author = document.getElementById('author');
+    read = document.getElementById('read')
+
+    // if (read) {
+    //     read.value = true;
+    // } else {
+    //     read.value = false;
+    // }
+    addBookToLibrary(title.value, author.value, read.checked);
+    title.value = '';
+    author.value = '';
+     e.preventDefault();
+})
+
+function Book (title, author, isRead) {
+    if(!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor")
+    }
+    this.title = title,
+    this.author = author,
+    this.isRead = isRead
+}
+
+// constructor test
+// const book1 = new Book ('lord of the rings', 'J.RR')
+// book1.isRead()
+
+
+function addBookToLibrary (title, author, isRead) {
+    const newBook = new Book(title, author, isRead)
+    myLibrary.push(newBook);
+    renderLibrary();
+    console.log(myLibrary)
+}
+
+// addBookToLibraryTest
+// addBookToLibrary('dune','frank herbert')
+// addBookToLibrary('1984','George Orwell')
+
+
+function renderLibrary () {
+    const bookDisplay = document.querySelector('.book-display');
+    myLibrary.forEach(function(book) {
+        const createBook = document.createElement('div');
+        createBook.classList.add('book');
+        createBook.textContent = `${book.title} authored by ${book.author} The book is ${book.isRead}`
+        bookDisplay.appendChild(createBook)
+    })
+}
+
+// renderLibrary test (comment out all other functions and leave array)
+// renderLibrary()
